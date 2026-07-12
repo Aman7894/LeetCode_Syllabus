f73f44085f58e1f5058ee1b39b2c36db34c00fae
Aman7894 <153308557+Aman7894@users.noreply.github.com>
2026-07-12 15:22:53 +0530

    Time: 7 ms (46.86%), Space: 35.4 MB (88.89%) - LeetHub

---
+++ 1881-maximum-value-after-insertion/1881-maximum-value-after-insertion.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string maxValue(string n, int x) {
+        char ch = x + '0';
+
+        if (n[0] == '-') {
+            for (int i = 1; i < n.size(); i++) {
+                if (n[i] - '0' > x) {
+                    n.insert(i, 1, ch);
+                    return n;
+                }
+            }
+            n.push_back(ch);
+            return n;
+        }
+
+        for (int i = 0; i < n.size(); i++) {
+            if (n[i] - '0' < x) {
+                n.insert(i, 1, ch);
+                return n;
+            }
+        }
+
+        n.push_back(ch);
+        return n;
+    }
+};
